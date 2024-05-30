d0b07d54a0922709b1e7a8a02f43263e7c04cc13
Anjula Perera <anjulaperera98@gmail.com>
2024-05-30 10:08:29 +0530

    added new files

---
+++ models/chat.js
@@ -0,0 +1,19 @@
+// models/Chat.js
+const mongoose = require("mongoose");
+
+const ChatSchema = new mongoose.Schema({
+  userMessage: {
+    type: String,
+    required: true,
+  },
+  botResponse: {
+    type: String,
+    required: true,
+  },
+  timestamp: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Chat", ChatSchema);
